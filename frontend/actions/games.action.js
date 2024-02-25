65d23b09e31ef9ba2fcb12974a6b1f658c31a2fd
KLEIN Jean-Daniel <kleinjeandaniel@gmail.com>
2024-02-25 13:49:05 +0100

    2

--- frontend/actions/games.action.js
+++ frontend/actions/games.action.js
@@ -5,7 +5,7 @@ export const SET_GAMES_LIST = "SET_GAMES_LIST";
 export const fetchGamesList = () => {
   return (dispatch) => {
     axios
-      .get(`https://animunjour.netlify.app/games/all-games`)
+      .get(`https://main--animunjour.netlify.app//games/all-games`)
       .then((res) => {
         let gamesList = res.data;
         dispatch({ type: SET_GAMES_LIST, payload: gamesList });
