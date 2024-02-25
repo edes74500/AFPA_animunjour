9dabde9bca9fd19f1695f1211f1c9f09316bbb57
KLEIN Jean-Daniel <kleinjeandaniel@gmail.com>
2024-02-25 13:00:53 +0100

    2

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,14 @@ app.use(cors());
 connectDB();
 
 //middleware
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", "true"); // Ajoutez cet en-tête
+  next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
